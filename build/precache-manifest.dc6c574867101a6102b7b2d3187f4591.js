87cbd5dbdabc2cf4b06f71fe937b0283a8d2ff6b
MohamedMagdyTajawal <mohamed.magdy@tajawal.com>
2019-01-20 19:55:12 +0200

    update read-me

---
+++ build/precache-manifest.dc6c574867101a6102b7b2d3187f4591.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e1847b424d5a8c354ed3",
+    "url": "/static/js/main.e1847b42.chunk.js"
+  },
+  {
+    "revision": "770c00542e895445dd59",
+    "url": "/static/js/1.770c0054.chunk.js"
+  },
+  {
+    "revision": "e1847b424d5a8c354ed3",
+    "url": "/static/css/main.d805506a.chunk.css"
+  },
+  {
+    "revision": "f38df5992bdb90cc33e9ce155818f286",
+    "url": "/index.html"
+  }
+];
